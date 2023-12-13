41c80e7d063f9bfe7f10fbc00f3e4422ecce610e
Michael Bestas <mkbestas@lineageos.org>
2023-12-13 04:25:20 +0200

    cheetah: Regenerate makefiles
    Change-Id: I1b958000a948050eec15fbf71da5b21e844fe221

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),cheetah)
 
-$(call add-radio-file,radio/abl.img)
-$(call add-radio-file,radio/bl1.img)
-$(call add-radio-file,radio/bl2.img)
-$(call add-radio-file,radio/bl31.img)
-$(call add-radio-file,radio/gsa.img)
-$(call add-radio-file,radio/ldfw.img)
-$(call add-radio-file,radio/modem.img)
-$(call add-radio-file,radio/pbl.img)
-$(call add-radio-file,radio/tzsw.img)
+$(call add-radio-file-sha1-checked,radio/abl.img,28aa16e75347ac29273c42527c9b9fc292aa6787)
+$(call add-radio-file-sha1-checked,radio/bl1.img,84a23a176788da33072a992d6423514584068cce)
+$(call add-radio-file-sha1-checked,radio/bl2.img,89e83b696dfe806e07edc95085edd3d858124edf)
+$(call add-radio-file-sha1-checked,radio/bl31.img,cbe712681df6dc400781b36ee34a3361d328b22e)
+$(call add-radio-file-sha1-checked,radio/gsa.img,6e7eef3534fdc52aa197817fdabd1fc3a7148c05)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,bcde7fe7db80a073166935cc05bb756ac19b6af1)
+$(call add-radio-file-sha1-checked,radio/modem.img,69eda14708197f30b8b680d15c855cad2371f4f2)
+$(call add-radio-file-sha1-checked,radio/pbl.img,d74d39e29d4be9d9868cd45b417f189858e4b03e)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,eea9a2d9a85d4b237af4635ca231f3eb22f702e5)
 
 endif
