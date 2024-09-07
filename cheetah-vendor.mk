5f1cce76ed4ffa072d5a68bba51263959bd1fc18
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 02:30:39 +0300

    cheetah: Add xml suffix to prebuilt_etc_xml modules
    Change-Id: I75a6d6f01f8e35df4a8ad862a96db00d3dc9091e

--- cheetah-vendor.mk
+++ cheetah-vendor.mk
@@ -119,7 +119,7 @@ PRODUCT_PACKAGES += \
     com.google.android.camerax.extensions \
     google-ril \
     oemrilhook \
-    vendor.google.edgetpu_app_service@1.0-service \
+    vendor.google.edgetpu_app_service@1.0-service.xml \
     vendor.google.edgetpu_app_service@1.0-service
 
 PRODUCT_PACKAGES += \
@@ -1966,29 +1966,29 @@ PRODUCT_PACKAGES += \
     com.google.pixel.euicc.update \
     com.google.pixel.wifi.ext \
     com.google.android.camera.experimental2022 \
-    android.hardware.authsecret-service.citadel \
-    android.hardware.camera.provider@2.7-service-google-apex \
-    android.hardware.confirmationui-service.trusty.vendor \
-    android.hardware.contexthub-service.generic \
-    android.hardware.gnss@2.1-service-brcm \
-    android.hardware.identity.strongbox \
-    android.hardware.neuralnetworks@service-darwinn-aidl \
-    android.hardware.oemlock-service.citadel \
-    android.hardware.security.keymint-service.citadel \
-    android.hardware.weaver-service.citadel \
-    com.google.edgetpu.dba-service \
-    com.google.edgetpu.tachyon-service \
-    dmd \
-    fingerprint-goodix \
-    manifest_input.processor-service \
-    manifest_radioext \
-    manifest_wifi_ext_aidl \
-    qorvo.uwb-service \
-    vendor.dolby.media.c2@1.0-service \
-    vendor.google.battery_mitigation-default \
-    vendor.google.edgetpu_vendor_service@1.0-service \
-    vendor.google.google_battery-default \
-    vendor.google.wireless_charger-default \
+    android.hardware.authsecret-service.citadel.xml \
+    android.hardware.camera.provider@2.7-service-google-apex.xml \
+    android.hardware.confirmationui-service.trusty.vendor.xml \
+    android.hardware.contexthub-service.generic.xml \
+    android.hardware.gnss@2.1-service-brcm.xml \
+    android.hardware.identity.strongbox.xml \
+    android.hardware.neuralnetworks@service-darwinn-aidl.xml \
+    android.hardware.oemlock-service.citadel.xml \
+    android.hardware.security.keymint-service.citadel.xml \
+    android.hardware.weaver-service.citadel.xml \
+    com.google.edgetpu.dba-service.xml \
+    com.google.edgetpu.tachyon-service.xml \
+    dmd.xml \
+    fingerprint-goodix.xml \
+    manifest_input.processor-service.xml \
+    manifest_radioext.xml \
+    manifest_wifi_ext_aidl.xml \
+    qorvo.uwb-service.xml \
+    vendor.dolby.media.c2@1.0-service.xml \
+    vendor.google.battery_mitigation-default.xml \
+    vendor.google.edgetpu_vendor_service@1.0-service.xml \
+    vendor.google.google_battery-default.xml \
+    vendor.google.wireless_charger-default.xml \
     aocd \
     aocxd \
     bipchmgr \
